a2b65ee7314e4953b617890903d71e176c524c21
valeen-rodriguez <valentinarodriguezkittler@gmail.com>
2024-06-18 15:12:43 -0300

    adding & fixing: more styling on input and image errors

--- js/script.js
+++ js/script.js
@@ -1,238 +1,224 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const songs = [
-        // JSON canciones
-        {
-            "id": 1,
-            "name": "Lovers Rock - TV Girl",
-            "image": "image/lovers-rock-img.png",
-            "audio": "audio/lovers-rock-audio.mp3"
-        },
-        {
-            "id": 2,
-            "name": "Something about you - Eyedress",
-            "image": "image/something-about-you-img.jpg",
-            "audio": "audio/something-about-you.mp3"
-        },
-        {
-            "id": 3,
-            "name": "Can I call you tonight? - Dayglow",
-            "image": "image/call-you-tonight-img.png",
-            "audio": "audio/call-you-tonight.mp3"
-        },
-        {
-            "id": 4,
-            "name": "I wanna be yours - Arctic Monkeys",
-            "image": "image/wanna-be-yours-img.png",
-            "audio": "audio/wanne-be-yours.mp3"
-        },
-        {
-            "id": 5,
-            "name": "Mr loverman - Ricky Montgomery",
-            "image": "image/mrs-loverman-img.png",
-            "audio": "audio/mr-loverman.mp3"
-        },
-        {
-            "id": 6,
-            "name": "Tek it - Cafuné",
-            "image": "image/tek-it-img.jpg",
-            "audio": "audio/tek-it.mp3"
-        }
-    ];
+    const jsonUrl = '/json/songs.json';
+    let songs = [];
 
-    const musicPlayer = document.getElementById("music-player");
-    const songImage = document.getElementById("song-image");
-    const songTitle = document.getElementById("song-title");
-    const welcomeText = document.getElementById("welcome-text");
-    const nameInputContainer = document.getElementById("name-input-container");
-    const screen = document.getElementById("screen");
-    const backButton = document.createElement("button");
-    backButton.textContent = "Volver Atrás";
-    backButton.id = "back-to-welcome";
-    backButton.style.display = "none";
-    screen.appendChild(backButton);
-
-    let currentSongIndex = -1;
-    let isPlaying = false;
-    let isSongDetailsView = false;
-
-    document.getElementById("submit-name").addEventListener("click", function() {
-        const nameInput = document.getElementById("name-input");
-        const userName = nameInput.value.trim();
-        if (userName) {
-            welcomeText.textContent = `Hola, ${userName}!`;
-            nameInputContainer.style.display = "none";
-            showSongListButton();
-        }
-    });
-
-    function showSongListButton() {
-        const showListButton = document.createElement("button");
-        showListButton.textContent = "Ver Lista de Canciones";
-        showListButton.id = "show-song-list";
-        screen.appendChild(showListButton);
-
-        showListButton.addEventListener("click", function() {
-            showSongList();
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(data => {
+            songs = data;
+            initialize();
+        })
+        .catch(error => {
+            console.error('un ERROR de JSON:', error);
         });
-    }
 
-    function showSongList() {
-        isSongDetailsView = false; // vista de playlist
-        backButton.style.display = "block";
-        const songListContainer = document.createElement("div");
-        songListContainer.classList.add("song-list-container");
+    function initialize() {
+        const musicPlayer = document.getElementById("music-player");
+        const songImage = document.getElementById("song-image");
+        const songTitle = document.getElementById("song-title");
+        const welcomeText = document.getElementById("welcome-text");
+        const nameInputContainer = document.getElementById("name-input-container");
+        const screen = document.getElementById("screen");
+        const backButton = document.createElement("button");
+        backButton.textContent = "Volver Atrás";
+        backButton.id = "back-to-welcome";
+        backButton.style.display = "none";
+        screen.appendChild(backButton);
 
-        const songList = document.createElement("ul");
-        songList.classList.add("song-list");
+        let currentSongIndex = -1;
+        let isPlaying = false;
+        let isSongDetailsView = false;
+        let isInPlaylistView = false;  // controla si estamos en la vista de la lista de canciones
 
-        songs.forEach(song => {
-            const listItem = document.createElement("li");
-            listItem.classList.add("song-item");
-            const link = document.createElement("a");
-            link.href = "#";
-            link.textContent = `${song.id}. ${song.name}`;
-            link.dataset.id = song.id;
-            link.addEventListener("click", function(event) {
-                event.preventDefault();
-                showSongDetails(song);
+        document.getElementById("submit-name").addEventListener("click", function() {
+            const nameInput = document.getElementById("name-input");
+            const userName = nameInput.value.trim();
+            if (userName) {
+                welcomeText.textContent = `Hola, ${userName}!`;
+                nameInputContainer.style.display = "none";
+                showSongListButton();
+            }
+        });
+
+        function showSongListButton() {
+            const showListButton = document.createElement("button");
+            showListButton.textContent = "Ver Lista de Canciones";
+            showListButton.id = "show-song-list";
+            screen.appendChild(showListButton);
+
+            showListButton.addEventListener("click", function() {
+                showSongList();
             });
-            listItem.appendChild(link);
-            songList.appendChild(listItem);
-        });
+        }
 
-        songListContainer.appendChild(songList);
-        screen.style.backgroundImage = "";
-        screen.innerHTML = "";
-        screen.appendChild(songListContainer);
-    }
+        function showSongList() {
+            isSongDetailsView = false; // vista de playlist
+            isInPlaylistView = true;  // estamos en la vista de la lista de canciones
+            backButton.style.display = "block";
+            const songListContainer = document.createElement("div");
+            songListContainer.classList.add("song-list-container");
 
-    function showSongDetails(song) {
-        isSongDetailsView = true; // detalle de la cancion
-        const songDetails = document.createElement("div");
-        songDetails.classList.add("song-details");
+            const songList = document.createElement("ul");
+            songList.classList.add("song-list");
 
-        const title = document.createElement("h3");
-        title.textContent = song.name;
-        songDetails.appendChild(title);
+            songs.forEach(song => {
+                const listItem = document.createElement("li");
+                listItem.classList.add("song-item");
+                const link = document.createElement("a");
+                link.href = "#";
+                link.textContent = `${song.id}. ${song.name}`;
+                link.dataset.id = song.id;
+                link.addEventListener("click", function(event) {
+                    event.preventDefault();
+                    showSongDetails(song);
+                });
+                listItem.appendChild(link);
+                songList.appendChild(listItem);
+            });
 
-        const image = document.createElement("img");
-        image.src = song.image;
-        image.alt = song.name;
-        songDetails.appendChild(image);
+            songListContainer.appendChild(songList);
+            screen.style.backgroundImage = "";
+            screen.innerHTML = "";
+            screen.appendChild(songListContainer);
+        }
 
-        const backToListButton = document.createElement("button");
-        backToListButton.textContent = "Volver Atrás";
-        backToListButton.addEventListener("click", function() {
-            showSongList();
-        });
-        songDetails.appendChild(backToListButton);
+        function showSongDetails(song) {
+            isSongDetailsView = true; // detalle de la cancion
+            const songDetails = document.createElement("div");
+            songDetails.classList.add("song-details");
 
-        screen.style.backgroundImage = `url(${song.image})`;
-        screen.style.backgroundSize = "cover";
-        screen.style.backgroundPosition = "center";
+            const title = document.createElement("h3");
+            title.textContent = song.name;
+            songDetails.appendChild(title);
 
-        screen.innerHTML = "";
-        screen.appendChild(songDetails);
+            const image = document.createElement("img");
+            image.src = song.image;
+            image.alt = song.name;
+            songDetails.appendChild(image);
 
-        currentSongIndex = songs.findIndex(s => s.id === song.id);
-        updateCurrentSong();
-    }
+            const backToListButton = document.createElement("button");
+            backToListButton.textContent = "Volver Atrás";
+            backToListButton.addEventListener("click", function() {
+                showSongList();
+            });
+            songDetails.appendChild(backToListButton);
 
-    backButton.addEventListener("click", function() {
-        showSongList();
-    });
-
-    function updateCurrentSong() {
-        if (currentSongIndex !== -1) {
-            const currentSong = songs[currentSongIndex];
-            musicPlayer.src = currentSong.audio;
-            if (isPlaying) {
-                musicPlayer.play();
-            } else {
-                musicPlayer.pause();
-            }
-            songTitle.textContent = currentSong.name;
-
-            if (isSongDetailsView) {
-                songImage.src = currentSong.image; // actualiza la imagen (solo si estamos detalle de la cancion)
-            }
-
-            if (document.querySelector(".song-details")) {
-                const titleElement = document.querySelector(".song-details h3");
-                const imageElement = document.querySelector(".song-details img");
-
-                if (titleElement && imageElement) {
-                    titleElement.textContent = currentSong.name;
-                    imageElement.src = currentSong.image;
-                    imageElement.alt = currentSong.name;
-                }
-            }
-
-            // actualiza imagen de fondo
-            screen.style.backgroundImage = `url(${currentSong.image})`;
+            screen.style.backgroundImage = `url(${song.image})`;
             screen.style.backgroundSize = "cover";
             screen.style.backgroundPosition = "center";
-        } else {
-            musicPlayer.pause();
-            songTitle.textContent = "";
+
+            screen.innerHTML = "";
+            screen.appendChild(songDetails);
+
+            currentSongIndex = songs.findIndex(s => s.id === song.id);
+            updateCurrentSong();
         }
-    }
 
-    document.getElementById("center").addEventListener("click", function() {
-        if (currentSongIndex !== -1) {
-            togglePlay();
-        }
-    });
+        backButton.addEventListener("click", function() {
+            showSongList();
+        });
 
-    document.getElementById("vol-up").addEventListener("click", function() {
-        changeVolume("up");
-    });
+        function updateCurrentSong() {
+            if (currentSongIndex !== -1) {
+                const currentSong = songs[currentSongIndex];
+                musicPlayer.src = currentSong.audio;
+                if (isPlaying) {
+                    musicPlayer.play();
+                } else {
+                    musicPlayer.pause();
+                }
+                songTitle.textContent = currentSong.name;
 
-    document.getElementById("vol-down").addEventListener("click", function() {
-        changeVolume("down");
-    });
+                if (isSongDetailsView) {
+                    songImage.src = currentSong.image; // actualiza la imagen (solo si estamos detalle de la cancion)
+                }
 
-    document.getElementById("prev").addEventListener("click", function() {
-        changeSong("prev");
-    });
+                if (document.querySelector(".song-details")) {
+                    const titleElement = document.querySelector(".song-details h3");
+                    const imageElement = document.querySelector(".song-details img");
 
-    document.getElementById("next").addEventListener("click", function() {
-        changeSong("next");
-    });
+                    if (titleElement && imageElement) {
+                        titleElement.textContent = currentSong.name;
+                        imageElement.src = currentSong.image;
+                        imageElement.alt = currentSong.name;
+                    }
+                }
 
-    function togglePlay() {
-        if (isPlaying) {
-            musicPlayer.pause();
-        } else {
-            musicPlayer.play();
-        }
-        isPlaying = !isPlaying;
-    }
-
-    function changeVolume(direction) {
-        const currentVolume = musicPlayer.volume;
-        if (direction === "up") {
-            if (currentVolume < 1) {
-                musicPlayer.volume += 0.1;
+                // actualiza imagen de fondo
+                screen.style.backgroundImage = `url(${currentSong.image})`;
+                screen.style.backgroundSize = "cover";
+                screen.style.backgroundPosition = "center";
+            } else {
+                musicPlayer.pause();
+                songTitle.textContent = "";
             }
-        } else if (direction === "down") {
-            if (currentVolume > 0) {
-                musicPlayer.volume -= 0.1;
+        }
+
+        // solamente las canciones se reproducen en la lista de canciones
+        document.getElementById("center").addEventListener("click", function() {
+            if (isInPlaylistView && currentSongIndex !== -1) {
+                togglePlay();
+            }
+        });
+
+        document.getElementById("vol-up").addEventListener("click", function() {
+            if (isInPlaylistView) {
+                changeVolume("up");
+            }
+        });
+
+        document.getElementById("vol-down").addEventListener("click", function() {
+            if (isInPlaylistView) {
+                changeVolume("down");
+            }
+        });
+
+        document.getElementById("prev").addEventListener("click", function() {
+            if (isInPlaylistView) {
+                changeSong("prev");
+            }
+        });
+
+        document.getElementById("next").addEventListener("click", function() {
+            if (isInPlaylistView) {
+                changeSong("next");
+            }
+        });
+
+        function togglePlay() {
+            if (isPlaying) {
+                musicPlayer.pause();
+            } else {
+                musicPlayer.play();
+            }
+            isPlaying = !isPlaying;
+        }
+
+        function changeVolume(direction) {
+            const currentVolume = musicPlayer.volume;
+            if (direction === "up") {
+                if (currentVolume < 1) {
+                    musicPlayer.volume += 0.1;
+                }
+            } else if (direction === "down") {
+                if (currentVolume > 0) {
+                    musicPlayer.volume -= 0.1;
+                }
             }
         }
-    }
 
-    function changeSong(direction) {
-        if (songs.length === 0) return;
+        function changeSong(direction) {
+            if (songs.length === 0) return;
 
-        if (direction === "prev") {
-            currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-        } else if (direction === "next") {
-            currentSongIndex = (currentSongIndex + 1) % songs.length;
+            if (direction === "prev") {
+                currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+            } else if (direction === "next") {
+                currentSongIndex = (currentSongIndex + 1) % songs.length;
+            }
+
+            updateCurrentSong();
         }
 
         updateCurrentSong();
     }
-
-    updateCurrentSong();
 });
